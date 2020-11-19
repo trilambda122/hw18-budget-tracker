a377e6aced554f3eccddb2361f01854689aa566c
trilambda122 <trilambda122>
2020-11-19 12:14:21 -0600

    inital commit of working app and push to heroku

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+const dotenv = require('dotenv').config();
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+const uri = process.env.ATLAS_URI;
+// connect to the database
+mongoose.connect(process.env.MONGODB_URI || uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
