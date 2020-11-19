c8a7080edb863f86c9113c8c3e548be385f56c4e
trilambda122 <trilambda122>
2020-11-19 12:19:58 -0600

    inital commit of working app and push to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 const dotenv = require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
