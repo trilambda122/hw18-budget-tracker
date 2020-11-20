76a18a23c1eb6ce9fd7bbf511623f883c5737fe2
trilambda122 <trilambda122>
2020-11-19 18:00:59 -0600

    back to just workbox

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("distro"));
+app.use(express.static("public"));
 
 // mongoose.connect("mongodb://localhost/budget", {
 //   useNewUrlParser: true,
--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "globDirectory": "distro/",
+  "globDirectory": "public/",
   "globPatterns": [
     "**/*.{png,html,js,css}"
   ],
